87e42b116e153c4c555db2f25d9dd1ed0bd6802a
tabbatharenea@gmail.com <tabbatharenea@gmail.com>
2018-05-21 20:32:59 -0400

    fixed delete bug in which incorrect item was being removed if items were filtered: passed the item in deleteItem() and then used .indexOf to locate correct index

--- controller.js
+++ controller.js
@@ -3,6 +3,7 @@ function TodoCtrl(){
   console.log("controller working");
   
   const vm = this; //vm short for view model
+  //this array contains the list of tasks, each of which are objects containing values for the task itself as a string, as well as a boolean for completed. Every newly added item defaults to completed:false
   vm.list = [
       {
         task: "Walk the dog",
@@ -27,21 +28,23 @@ function TodoCtrl(){
   ]
 
   vm.addItem = function(input) {
-
+    //set up a new object using the arguments passed in from the view
     vm.newTodo = {
       task: input,
       completed: false
     }
-
+    //push that new object into the list of todos array
     vm.list.push(vm.newTodo);
+    //reset the value on the input field
     vm.input = "";
-    console.log(vm.newTodo);
-    
   }
 
-  vm.deleteItem = function($index, item) {
-    console.log($index, item);
-    vm.list.splice($index, 1);
+  vm.deleteItem = function(item) {
+    console.log(`item name is: ${item.task}`);
+    let index = vm.list.indexOf(item);
+    console.log(`the index is ${index}`);
+    vm.list.splice(index, 1);
+    
   }
 
   vm.completeItem = function($index, item){
